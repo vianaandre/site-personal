1cfa2a10cd162f27ebddeb5495884b3f81a7fa9f
André Viana <andredapperviana@gmail.com>
2021-09-17 14:39:05 -0300

    Update sendEmail.ts

--- src/services/sendEmail.ts
+++ src/services/sendEmail.ts
@@ -10,7 +10,7 @@ const SendEmail = async (data: SendProps): Promise<unknown> => {
 
   console.log(data)
 
-  await fetch('http://https://app-backend-send-email.herokuapp.com/send', {
+  await fetch('http://app-backend-send-email.herokuapp.com/send', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
